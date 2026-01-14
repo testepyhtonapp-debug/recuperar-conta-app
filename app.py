cf7f0c4e775f46252a958dff3070c9fde3ac85a3
testepyhtonapp-debug <testepyhtonapp@gmail.com>
2026-01-14 18:48:39 +0000

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Servidor de recuperação ativo"
+    return "status ok"
 
-@app.route("/status")
-def status():
-    return jsonify({"status": "ok"})
+@app.route("/recuperar")
+def recuperar():
+    return "Página de recuperação (escolher utilizador ou password)"
+
+@app.route("/recuperar/password")
+def recuperar_password():
+    return "Recuperação de password"
+
+@app.route("/recuperar/utilizador")
+def recuperar_utilizador():
+    return "Recuperação de utilizador"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
