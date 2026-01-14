7816efe215ab69c9ace247799d89299885b137c1
testepyhtonapp-debug <testepyhtonapp@gmail.com>
2026-01-14 18:43:05 +0000

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Servidor de recuperação ativo"
+
+@app.route("/status")
+def status():
+    return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
